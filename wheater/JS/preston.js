3e6068c308b0228662580f349a9f08c64aca4132
juliagarciamaestre <juliagarciamaestre@gmail.com>
2022-03-12 04:41:35 -0300

    submit changes

--- wheater/JS/preston.js
+++ wheater/JS/preston.js
@@ -28,10 +28,9 @@ document.querySelector('#Month').textContent = actualMonth
 document.querySelector('#Year').textContent = actualYear
 console.log(actualDay)
 
-// const specialMessage = document.querySelector('.specialmessage');
-// let friday = days[5]
+const specialMessage = document.querySelector('.specialmessage');
+let friday = days[5]
 
 if (actualDay != friday){
         document.querySelector('.specialmessage').style.display ='none'
-
     }
