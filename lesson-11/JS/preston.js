872aa87b36947481f8778a2c44ea07c4f47b2390
juliagarciamaestre <juliagarciamaestre@gmail.com>
2022-03-30 15:10:13 -0300

    create new folder

---
+++ lesson-11/JS/preston.js
@@ -0,0 +1,36 @@
+const homeButton = document.querySelector('.home');
+const menuNav = document.querySelector('.menunavigation')
+
+homeButton.addEventListener('click', () => {menuNav.classList.toggle('responsive')}, false);
+
+// To solve the mid resizing issue with responsive class on
+window.onresize = () => {if (window.innerWidth > 760) menuNav.classList.remove('responsive')};
+
+let lastModif= new Date(document.lastModified);
+// console.log(lastModif)
+
+var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
+var months = ['January','February','March','April','May','June','July','August','September','October','November','December']
+
+let actualDay = days[lastModif.getDay()]
+let acutalDate = lastModif.getDate()
+let actualMonth = months[lastModif.getMonth()]
+let actualYear = lastModif.getFullYear()
+// console.log(actualDay)
+// console.log(acutalDate)
+// console.log(actualMonth)
+// console.log(actualYear)
+
+
+document.querySelector('#Day').textContent = actualDay
+document.querySelector('#Date').textContent = acutalDate
+document.querySelector('#Month').textContent = actualMonth
+document.querySelector('#Year').textContent = actualYear
+console.log(actualDay)
+
+const specialMessage = document.querySelector('.specialmessage');
+let friday = days[5]
+
+if (actualDay != friday){
+        document.querySelector('.specialmessage').style.display ='none'
+    }
---
+++ lesson-11/lesson-5/cards.js
@@ -0,0 +1,42 @@
+const requestURL = "https://byui-cit230.github.io/weather/data/towndata.json";
+fetch(requestURL)
+    .then(function(response){
+        return response.json()
+    })
+    .then(function(jsonObject){
+        const towns = jsonObject["towns"].filter((town) =>{
+            if(town.name === "Preston"){
+                return town;
+            }
+            if (town.name === "Fish Haven") {
+                return town;
+            }
+            if (town.name === "Soda Springs"){
+                return town;
+            }
+        });
+        
+        for (let i =0; i < towns.length; i++ ) {
+            let card = document.createElement("section");
+            let h1 = document.createElement("h1");
+            h1.textContent = towns[i].name;
+            card.appendChild(h1);
+            document.querySelector("div.cards").appendChild(card);
+            let h4 = document.createElement("h4");
+            h4.textContent = towns[i].motto;
+            card.appendChild(h4)
+            let p = document.createElement("p");
+            p.textContent = "Year Founded: " + towns[i].yearFounded;
+            card.appendChild(p)
+            let p2 = document.createElement("p");
+            p2.textContent = "Population: " + towns[i].currentPopulation;
+            card.appendChild(p2)
+            let p3 = document.createElement("p");
+            p3.textContent = "Annual Rain Fall: " + towns[i].averageRainfall;
+            card.appendChild(p3)
+            let img = document.createElement("img");
+            img.setAttribute("src", "lesson-5/" + towns[i].photo);
+            img.setAttribute("alt", towns[i].name + " photo");
+            card.appendChild(img)
+        }
+    })
