5f29d0b6dd6f1d54fc47df77a3a405a6187e79c2
Julia García <juliagarciamaestre@gmail.com>
2022-04-05 03:29:04 -0400

    create scoots site

---
+++ scooter/JS/scoots.js
@@ -0,0 +1,51 @@
+const homeButton = document.querySelector('.home');
+const menuNav = document.querySelector('.menunavigation')
+
+homeButton.addEventListener('click', () => {menuNav.classList.toggle('responsive')}, false);
+
+// To solve the mid resizing issue with responsive class on
+window.onresize = () => {if (window.innerWidth > 760) menuNav.classList.remove('responsive')};
+
+// let lastModif= new Date(document.lastModified);
+// // console.log(lastModif)
+
+// var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
+// var months = ['January','February','March','April','May','June','July','August','September','October','November','December']
+
+// let actualDay = days[lastModif.getDay()]
+// let acutalDate = lastModif.getDate()
+// let actualMonth = months[lastModif.getMonth()]
+// let actualYear = lastModif.getFullYear()
+// // console.log(actualDay)
+// // console.log(acutalDate)
+// // console.log(actualMonth)
+// // console.log(actualYear)
+
+
+// document.querySelector('#Day').textContent = actualDay
+// document.querySelector('#Date').textContent = acutalDate
+// document.querySelector('#Month').textContent = actualMonth
+// document.querySelector('#Year').textContent = actualYear
+// // console.log(actualDay)
+
+// const specialMessage = document.querySelector('.specialmessage');
+// let friday = days[5]
+
+// if (actualDay != friday){
+//         document.querySelector('.specialmessage').style.display ='none'
+//     }
+
+
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=e359fca7add1cc64c4539c515a2afe5b"
+fetch(apiURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        // console.log(jsObject);
+        const desc = jsObject.weather[0].description;
+        document.querySelector("#temperatura").textContent = desc;
+        const highT = jsObject.main.temp;
+        document.getElementById("High").textContent = highT + "°F";
+        const humidity = jsObject.main.humidity;
+        document.getElementById("humidity").textContent = humidity + "%";
+    })
+const forecastApiUrl ="https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=e359fca7add1cc64c4539c515a2afe5b"
