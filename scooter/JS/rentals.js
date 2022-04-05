77dd7393e6e6cb291af4b69a5ea9392e05c23c76
Julia García <juliagarciamaestre@gmail.com>
2022-04-05 17:17:36 -0400

    add images

--- scooter/JS/rentals.js
+++ scooter/JS/rentals.js
@@ -1,1 +1,16 @@
-const requestURL = 
+const requestdoc = "https://juliagarciamaestre.github.io/scooter/data/rentals.json" 
+fetch(requestdoc)
+    .then(function(response){
+        return response.json()
+    })
+    .then(function(jsonObject){
+        console.table(jsonObject);
+        const rentals = jsonObject["rentals"];
+        for (let i = 0; i < rentals.length; i++ ) {
+            let card = document.createElement("section");
+            let h2 = document.createElement("h2");
+            h2.textContent = rentals[i].rentaltype;
+            card.appendChild(h2);
+            document.querySelector("div.cards").appendChild(card);
+        };
+    })
