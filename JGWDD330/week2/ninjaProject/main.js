875a1d592500075c0b9dcfb3ea1befb7c2f1464e
Julia García <juliagarciamaestre@gmail.com>
2022-05-09 23:52:27 -0400

    add ninjaProject website

---
+++ JGWDD330/week2/ninjaProject/main.js
@@ -0,0 +1,5 @@
+alert("Welcome to Quiz Ninja!");
+const question = "What is Superman's real name?"
+const answer = prompt(question); // Allows the user to type in response (prompt)
+alert(`You answered ${answer}`);
+
