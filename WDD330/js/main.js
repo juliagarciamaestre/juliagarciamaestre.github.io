fdc58409be310bd13407f4d000f9fa467a35f90d
Julia García <juliagarciamaestre@gmail.com>
2022-09-16 17:39:17 -0400

    add some notes to week 1

--- WDD330/js/main.js
+++ WDD330/js/main.js
@@ -1,7 +1,21 @@
-var links = document.getElementById("order_list")
+//Get the element using the ID and store to a variable call list
+var list = document.getElementById("order_list")
+//Create an array with the links of the weeks
 const links = [
     {
         label: "Week1 notes",
         url: "week1/index.html",
     }
 ]
+
+//Using a for loop to create elements and create the list of the links.
+for (let i=0; i<links.length; i++){
+    let li = document.createElement("li");
+    let a = document.createElement("a");
+    a.textContent = links[i].label;
+    a.setAttribute("href", links[i].url);
+    li.appendChild(a);
+    list.appendChild(li);
+    console.log(li);
+}
+
