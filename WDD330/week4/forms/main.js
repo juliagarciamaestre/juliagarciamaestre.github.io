4734bcdf58e50e02615adb31aa8340658461b00f
Julia García <juliagarciamaestre@gmail.com>
2022-10-08 17:18:44 -0300

    add form exercise

---
+++ WDD330/week4/forms/main.js
@@ -0,0 +1,28 @@
+const form = document.forms['search'];
+const input = form.elements.searchInput;
+
+// input.addEventListener('focus', () => alert('focused'), false);
+
+// input.addEventListener('blur', () => alert('blurred'), false);
+
+// input.addEventListener('change', ()=>alert('changed'), false);
+
+
+form.addEventListener ('submit', search, false);
+
+function search(event) {
+    // alert('Form Submitted');
+    alert(`You Searched for: ${input.value}`);
+    event.preventDefault();
+}
+
+input.addEventListener('focus', function(){
+    if (input.value==='Search Here') {
+        input.value = '' 
+    }
+}, false);
+
+input.addEventListener('blur', function(){
+    if(input.value === '') {
+        input.value = 'Search Here';
+    } }, false);
