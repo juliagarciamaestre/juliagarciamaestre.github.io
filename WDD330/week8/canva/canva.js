5eefb2eac33394248a3ebb8eef187146990c6371
Julia García <juliagarciamaestre@gmail.com>
2022-11-05 16:35:18 -0300

    add week8

--- WDD330/week8/canva/canva.js
+++ WDD330/week8/canva/canva.js
@@ -45,7 +45,54 @@ function drawCircle(canvas) {
     context.stroke();
 }
 
-const canvas4 = document.getElementById('demo4');
+function saveDrawing() {
+    var canvas5 = document.getElementById("demo5");
+    window.open(canvas5.toDataURL("image/png"));
+}
+
+function drawImageToCanvas() {
+    var canvas = document.getElementById("demo6");
+    var context = canvas.getContext("2d");
+    var image = document.getElementById("myImageElem");
+    context.drawImage(image, 0, 0); 
+}
+
+function manipulateImage() {
+    var canvas = document.getElementById("demo7");
+    var context = canvas.getContext("2d");
+    var image = document.getElementById("secondImage");
+    context.drawImage(image, 68, 68);
+
+    var imageData = context.getImageData(0, 0, 200, 200);
+    
+    var red, green, blue, greyscale;
+    
+    for (var i = 0; i < imageData.data.length; i += 4) {
+    red = imageData.data[i];
+    green = imageData.data[i + 1];
+    blue = imageData.data[i + 2];
+
+    var grayscale = red * 0.3 + green * 0.59 + blue * 0.11;
+        
+    imageData.data[i] = grayscale;
+    imageData.data[i + 1] = grayscale;
+    imageData.data[i + 2] = grayscale;
+    } 
+    context.putImageData(imageData, 0, 0);
+}
+
+var button = document.getElementById("saveButton");
+button.addEventListener("click", saveDrawing, false);
+
+
 drawPattern();
 drawGradient();
+
+const canvas4 = document.getElementById('demo4');
+
 drawCircle(canvas4);
+const canvas5 = document.getElementById('demo5');
+drawCircle(canvas5);
+
+addEvent('load', window, drawImageToCanvas);
+addEvent('load', window, manipulateImage);
