f1443d16f7a79c4eb1b68c97c39e73f0ba9f695f
Julia García <juliagarciamaestre@gmail.com>
2022-11-05 16:18:39 -0300

    add week 8

--- WDD330/js/main.js
+++ WDD330/js/main.js
@@ -29,6 +29,10 @@ const links = [
     {
         label: "Week7 notes",
         url: "week7/index.html",
+    },
+    {
+        label: "Week8 notes",
+        url: "week8/index.html",
     }
 
 ]
---
+++ WDD330/week8/canva/canva.js
@@ -0,0 +1,51 @@
+var canvas = document.getElementById("myCanvas");     
+var context = canvas.getContext("2d");
+context.strokeStyle = "red";
+context.fillStyle = "rgba(0, 0, 255, 0.5)";
+context.fillRect(10, 10, 100, 100);   
+context.strokeRect(10, 10, 100, 100);
+
+function drawPattern() {
+    var canvas = document.getElementById("demo2");
+    var context = canvas.getContext("2d");
+    context.strokeStyle = "red";
+
+    var img = new Image(); 
+    img.src = "../images/bike_sprite.png";
+    img.onload = function() {
+        var pattern = context.createPattern(img, "repeat"); 
+        context.fillStyle = pattern;                        
+        context.fillRect(10, 10, 100, 100);                  
+        context.strokeRect(10, 10, 100, 100);             
+        };
+}
+
+function drawGradient() {
+    var canvas = document.getElementById('demo3');
+    var context = canvas.getContext("2d");
+    context.strokeStyle = "red";
+    var gradient = context.createLinearGradient(0, 0, 0, 200);
+    gradient.addColorStop(0, "blue"); 
+    gradient.addColorStop(1, "white"); 
+    context.fillStyle = gradient; 
+    context.fillRect(10, 10, 100, 100); 
+    context.strokeRect(10, 10, 100, 100); 
+}
+
+
+function drawCircle(canvas) {
+    var context = canvas.getContext("2d");
+    context.beginPath();
+    context.arc(50, 50, 30, 0, Math.PI*2, true);
+    context.closePath();
+    context.strokeStyle = "red";
+    context.fillStyle = "blue";
+    context.lineWidth = 3;
+    context.fill();
+    context.stroke();
+}
+
+const canvas4 = document.getElementById('demo4');
+drawPattern();
+drawGradient();
+drawCircle(canvas4);
