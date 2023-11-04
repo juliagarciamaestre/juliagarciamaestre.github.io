20da96398d94bf6336bf84226d497e1c298a669f
Julia García <juliagarciamaestre@gmail.com>
2023-11-04 17:45:03 -0300

    Create ToDo React Project

---
+++ ToDoProject/vite-project/src/todoList.jsx
@@ -0,0 +1,16 @@
+import { TodoItem } from "./todoItem"
+
+    
+export function TodoList({todos, toggleTodo, deleteTodos}){
+    return<ul className="list">
+    {todos.length === 0 && "No Todos"}
+    {todos.map(todo => {
+      return(
+        <TodoItem {...todo} key={todo.id} toggleTodo={toggleTodo}
+        deleteTodos={deleteTodos}/>
+    
+      )
+    })}
+    
+  </ul>
+}
